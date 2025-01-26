86a23d0427fa09fb18a7a8253ab31033224a2304
Wilman Conde <willconde2@hotmail.com>
2025-01-26 18:39:00 -0500

    --amend

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/index.scss'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './styles/index.scss';
+import App from './App.jsx';
+import Footer from './components/Footer.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Footer />
   </StrictMode>,
-)
+);
